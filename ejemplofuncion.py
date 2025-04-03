25b0d3e76e7b65f10b6e41f4f6211e5b5b214793
carlos2512a <carlos.villanueva18@inacapmail.cl>
2025-04-02 21:43:45 -0300

    Add files via upload

---
+++ ejemplofuncion.py
@@ -0,0 +1,11 @@
+def Saludar():
+    print('-----------------')
+    nombre=str(input('¿Cual es tu nombre? '))
+    apellido=str(input('¿Cual es tu apellido? '))
+    print('hola ',nombre,'',apellido, '¡1Bienvenido!!')
+    print('---------------------')
+    
+Saludar()
+Saludar()
+Saludar()
+
