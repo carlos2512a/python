3f77d4b4581c31fc52894a3c932da8c61f5959f5
carlos2512a <carlos.villanueva18@inacapmail.cl>
2025-09-26 21:12:51 -0300

    Update conexion.py
    Signed-off-by: carlos2512a <carlos.villanueva18@inacapmail.cl>

--- DAO/conexion.py
+++ DAO/conexion.py
@@ -1,1 +1,20 @@
+import pymysql
 
+
+class Conexion:
+    def __init__(self,host,usuario,clave,bd):
+        self.con = pymysql.connect(host=host,user=usuario,password=clave,db=bd)
+        self.cursor = self .con.cursor()
+    
+    def ejecutarQuery(self,sql):
+        self.cursor.execute(sql)
+        return self.cursor
+    
+    def desconectar(self):
+        self.con.close()
+        
+    def commit(self):
+        self.con.commit()
+        
+    def rollback(self):
+        self.con.rollback()
