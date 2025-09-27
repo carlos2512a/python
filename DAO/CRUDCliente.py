3d5940d5c656576f802a6d6a2a36e7513fcd4ace
carlos2512a <carlos.villanueva18@inacapmail.cl>
2025-09-26 21:12:28 -0300

    Update CRUDCliente.py
    Signed-off-by: carlos2512a <carlos.villanueva18@inacapmail.cl>

--- DAO/CRUDCliente.py
+++ DAO/CRUDCliente.py
@@ -1,1 +1,18 @@
+from conexion import Conexion
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+host = os.getenv("HOST")
+passwd = os.getenv("PASS")
+user = os.getenv("USER")
+db = os.getenv("DB")
+print (host)
+
+conexion1= Conexion(host,user,passwd,db)
+
+cursor = conexion1.ejecutarQuery("select * from cliente")
+datos = cursor.fetchall()
+print(datos)
+
+conexion1.desconectar()
