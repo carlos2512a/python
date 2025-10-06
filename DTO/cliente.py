653a136e8f6ac7da518c15a08b2a9925597e7d12
carlos2512a <carlos.villanueva18@inacapmail.cl>
2025-10-05 22:21:12 -0300

    Update cliente.py
    Signed-off-by: carlos2512a <carlos.villanueva18@inacapmail.cl>

--- DTO/cliente.py
+++ DTO/cliente.py
@@ -1,11 +1,26 @@
-from tipo import Tipo
-import os
-from dotenv import load_dotenv
+from DTO.socioNegocio import SocioNegocio 
+class Cliente(SocioNegocio):
+    def __init__(self, run,nombre,apellido, direccion, fono, correo,codigo ,tipo,montoCredito,deuda):
+        super().__init__(run,nombre, apellido,direccion,fono,correo)
+        self.__codigo = codigo
+        self.__tipo = tipo
+        self.__montoCredito = montoCredito
+        self.__deuda = deuda
+    
+    def __str__(self):
+        return f"Cliente de run:{self.getRun()}"
+    
+    def getCodigo(self):
+        return self.__codigo
+    
+    def getTipo(self):
+        return self.__tipo 
+    
+    def getmontoCredito(self):
+        return self.__montoCredito
+    
+    def getDeuda(self):
+        return self.__deuda
 
-
-load_dotenv()
-host = os.getenv("HOST")
-passwd = os.getenv("PASS")
-user = os.getenv("USER")
-db = os.getenv("DB")
-print (host)
+carlos = Cliente(12314,"Carlos","Apellido","Puente Alto",87987897,"carlos@carlos",1,1,600000,60000)
+print(carlos)
